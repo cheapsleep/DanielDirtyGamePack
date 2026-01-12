0cb7305c8fd17ae136542bc2528468fc52b23d1e
cheapsleep <dan.heystudios@gmail.com>
2026-01-11 19:08:49 -0800

    refactor: remove unused Campfire font injection from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,22 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import campfireUrl from './assets/fonts/Campfire.ttf?url'
-
-const injectedFontStyle = document.createElement('style')
-injectedFontStyle.textContent = `
-@font-face {
-  font-family: 'Campfire';
-  src: url('${campfireUrl}') format('truetype');
-  font-weight: 400;
-  font-style: normal;
-  font-display: swap;
-}
-html, body, #root, #root * {
-  font-family: 'Campfire', 'Carter One', system-ui, sans-serif !important;
-}
-`
-document.head.appendChild(injectedFontStyle)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
