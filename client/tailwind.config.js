74d5de1f8614bf05bb2b256514b4cacec95f4544
cheapsleep <dan.heystudios@gmail.com>
2026-01-11 19:05:32 -0800

    refactor: remove unused font family configuration from Tailwind CSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'campfire': ['Thertole', 'Campfire', 'sans-serif'],
-        'thertole': ['Thertole', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
